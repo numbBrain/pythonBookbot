8690eb201736444978135f876ecb60b85196ac1f
numbBrain <numbBrain>
2025-12-25 08:15:29 +0530

    added cli args

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_num_chars, sort_dict
 
 def get_book_text(filepath):
@@ -7,7 +8,13 @@ def get_book_text(filepath):
 
 def main():
 
-    content = get_book_text("./books/frankenstein.txt")
+    args = sys.argv
+    
+    if len(args) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    content = get_book_text(args[1])
     num_of_words = get_num_words(content)
     print(f"Found {num_of_words} total words")
     char_dict = get_num_chars(content)
